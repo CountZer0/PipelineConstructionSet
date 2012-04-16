0da1038f05512f2b6f0511e58705ec61d3f7eaf1
Jason Parks <Count.Zr0@gmail.com>
2012-04-15 20:44:27 -0700

    added some niceties
    untested to see if runs. Need more dependent modules

--- python/moBu/startup/pcsGlobalSetup.py
+++ python/moBu/startup/pcsGlobalSetup.py
@@ -1,46 +1,40 @@
 '''
 Author: jason
-Created: Apr 09, 2012
-Module: 
-Purpose: Adds paths to MoBu env
+Created: Apr 15, 2012
+Module: startup.pcsGlobalSetup
+Purpose: Adds paths to Mobu env
 '''
 
-import getpass
-import platform
-import re
-import sys
-import xml.etree.ElementTree as ET
-import inspect
+
+import getpass #@UnresolvedImport
 import os
+import platform #@UnresolvedImport
+import re #@UnresolvedImport
+import sys
+import xml.etree.ElementTree as ET #@UnresolvedImport
+import inspect #@UnresolvedImport
+
 
 PCSglobal = "/Users/jason/remotePCS/PCSglobal"	#Must match networkLoc path in gVarInit
 
+#--------------------------------------------
 # get user's main main Project tool path
 userXML = ET.parse("%s/data/%s/PCSuser.xml" % (PCSglobal, getpass.getuser()))
 userXMLcore = userXML.getiterator('Core')[0]
 userToolRoot = userXMLcore.get('PCSprojectToolPath')
-userTeam = userXMLcore.get('PCSactiveTeam')
 
+#--------------------------------------------
 # get extra relative paths to add for MotionBuilder
 globalXML = ET.parse("%s/installData/PCSstudio.xml" % PCSglobal)
 globalXMLcore = globalXML.getiterator('Core')[0]
 mobuRelPaths = eval(globalXMLcore.get('MoBuPaths'))
 
-# get team tool path
-try:
-	teamTBXML = ET.parse("%s/installData/%sTB.xml" % (PCSglobal, userTeam))
-	teamTBXMLcore = teamTBXML.getiterator('Core')[0]
-	teamTBroot = teamTBXMLcore.get('teamTBRoot')
-
-	# add Team ToolBox path
-	sys.path.append('%s/python' % teamTBroot)
-except: print 'No team menu setup yet'
-
+#--------------------------------------------
 # get bitVersion
 bit = 'win32'
 if platform.architecture()[0] == '64bit': bit = 'win64'
 
-
+#--------------------------------------------
 # add to sys.path
 for mobuRelPath in mobuRelPaths:
 	
@@ -52,25 +46,31 @@ for mobuRelPath in mobuRelPaths:
 	if re.search('PIL', mobuRelPath):
 		mobuRelPath = '%s/%s' % (mobuRelPath, bit)
 	
-	# add paths
+	# add paths from userXML toolpath
 	sys.path.append('%s/%s' % (userToolRoot, mobuRelPath))
 
-
-
-
+#--------------------------------------------
 # start debugging
 import diagnostic.wingdbstub #@UnusedImport
 
-from diagnostics.pcsLogger import moBuLogger
+from diagnostic.pcsLogger import moBuLogger
 
-# start PCS Global menu
-from moBu.sysGlobalMenu import MobuMenu
+#--------------------------------------------
+# do main import loop
 try:
-	MobuMenu().createTool()
+	from moBu.sysGlobalMenu import MobuArtMonkeyMenu
 except:
-	moBuLogger.error("Failed to create PCSglobal menu")
+	moBuLogger.info(sys.exc_info())
+	moBuLogger.errorDialog("Failed to import moBu and start ArtMonkey")
+	print "Failed to import moBu. Error: "
+	print sys.exc_info()
 
-#moBuLogger.info('Ran pcsGlobalSetup.py')
+#--------------------------------------------
+# start ArtMonkey Global menu
+try:
+	MobuArtMonkeyMenu().createTool()
+except:
+	moBuLogger.error("Failed to create Art Monkey menu")
 
 #--------------------------------------------
 # report run from location
