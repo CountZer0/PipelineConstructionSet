889a7008f77f1afd20629ed5cdb38a15bea500fe
JasonPC <Count.Zr0@gmail.com>
2012-04-16 21:11:42 -0700

    gVar test

---
+++ python/common/core/__init__.py
@@ -0,0 +1,9 @@
+'''
+Author: jason
+Created: Apr 16, 2012
+Module: core.__init__
+Purpose: to import 
+'''
+    
+    
+print "core.__init__ imported" 
---
+++ python/common/core/gVarInit.py
@@ -0,0 +1,3 @@
+teamA = 'GreatGameA'teamB = 'GreatGameB'teamC = 'GreatGameC'teamD = 'GreatGameD'
+#remoteLoc = '//networkDrive/artMonkey'#remoteLoc = 'C:/APremote/'remoteLoc = '/Users/jason/remoteAP/APglobal'
+emergency = 0
