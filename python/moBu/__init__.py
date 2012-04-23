6ba281c9f9f0357f1ab2fa5bb6c06652051e7fb3
JasonPC <Count.Zr0@gmail.com>
2012-04-22 21:34:13 -0700

    adding packages

---
+++ python/moBu/__init__.py
@@ -0,0 +1,9 @@
+'''
+Author: jason
+Created: Apr 22, 2012
+Module: moBu.__init__
+Purpose: to import moBu modules
+'''
+    
+    
+print "moBu.__init__ imported" 
---
+++ python/moBu/core/__init__.py
@@ -0,0 +1,9 @@
+'''
+Author: jason
+Created: Apr 22, 2012
+Module: moBu.core.__init__
+Purpose: to import core modules
+'''
+    
+    
+print "moBu.core.__init__ imported" 
